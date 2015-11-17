6adffe503cd250479a0de1b0f8976bbebdce9f16
Dimos <dimos.it21149@gmail.com>
2015-11-17 02:40:37 +0200

    update the manifest to work properly ther custom action check to do file to see how to create the intent

--- app/src/main/java/assignment1/android/hua/gr/assignment1/MainActivity.java
+++ app/src/main/java/assignment1/android/hua/gr/assignment1/MainActivity.java
@@ -1,9 +1,12 @@
 package assignment1.android.hua.gr.assignment1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -11,6 +14,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     @Override
