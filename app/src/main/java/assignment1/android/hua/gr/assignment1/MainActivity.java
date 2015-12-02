c3d24a848d29f56fcc29b60c2f16c52cf24e52ba
Dimos <dimos.it21149@gmail.com>
2015-12-02 20:28:46 +0200

    create layout of app

--- app/src/main/java/assignment1/android/hua/gr/assignment1/MainActivity.java
+++ app/src/main/java/assignment1/android/hua/gr/assignment1/MainActivity.java
@@ -1,12 +1,9 @@
 package assignment1.android.hua.gr.assignment1;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends ActionBarActivity {
 
